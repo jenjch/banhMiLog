dee163177fa482c197ef8dec0a8ce704b320d8cd
Jen C <jenjch8@gmail.com>
2020-02-01 14:12:16 -0600

    updated JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,30 @@ const mysql = require("mysql");
 // require pw in separate file using module.exports
 const pw = require("./pw.js");
 
+var connection;
 
-// set up MySQL connection.
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: pw,
-  database: "banhmi_db"
-});
+// JAWSDB, set up connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: pw,
+    database: "banhmi_db"
+  });
+}
+
+
+// // set up MySQL connection.
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: pw,
+//   database: "banhmi_db"
+// });
 
 // make connection
 connection.connect(function(err) {
@@ -23,5 +38,6 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 // export connection for our ORM to use.
 module.exports = connection;
